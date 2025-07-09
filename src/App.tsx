a67f11cb97ace3c7321fae953189ecf8d67a1afd
Vatsal <reachtovatsal@gmail.com>
2025-07-09 21:08:51 +0530

    React project startup + tailwind + antd

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className="bg-amber-200 h-[100vh] text-amber-800 text-2xl font-bold text-center">Redux-Toolkit</div>
+    </>
+  )
+}
+
+export default App
