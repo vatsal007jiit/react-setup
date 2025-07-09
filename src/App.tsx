75b3a9912b73efc1ee9c7cd66ca97a4868917b3f
Vatsal Gupta <72720227+vatsal007jiit@users.noreply.github.com>
2025-07-09 21:17:13 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <div className="bg-amber-200 h-[100vh] text-amber-800 text-2xl font-bold text-center">Redux-Toolkit</div>
+      <div className="bg-amber-200 h-[100vh] text-amber-800 text-2xl font-bold text-center">Welcome to React</div>
     </>
   )
 }
