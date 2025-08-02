ac9c0cbe01b2f3125c892ad0b06057e070cd1d93
Vatsal Gupta <72720227+vatsal007jiit@users.noreply.github.com>
2025-08-03 00:51:47 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <>
       <div className="bg-amber-200 h-[100vh] text-amber-800 text-2xl font-bold text-center">Welcome to React</div>
+      <ToastContainer position="top-center" autoClose={1000} />
     </>
   )
 }
